7591467092ece07b92a0d347e474c4087fd01cd8
Zoe Ames <zoeames@gmail.com>
2015-05-23 13:59:34 -0500

    needs added

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -11,4 +11,11 @@ controllers.controller('AboutController', ['$scope', function($scope){
 
 controllers.controller('NeedsController', ['$scope', function($scope){
   $scope.message = 'Hello I am the Needs PAGE';
+  $scope.needs=[];
+
+  $scope.addNeeds=function(thingy){
+    $scope.needs.push(thingy);
+    $scope.newNeed='';
+  };
+
 }]);
